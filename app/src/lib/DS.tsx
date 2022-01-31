364aaa4cb6325c822326b39a7ecace4f2beaa8fd
Chetan Satpute <chetan.satpute2002@gmail.com>
2022-02-01 03:22:13 +0530

    React router integration

--- app/src/lib/DS.tsx
+++ app/src/lib/DS.tsx
@@ -1,26 +1,25 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 
 enum DS {
   NodeArray = "Node Array",
   BinarySearchTree = "Binary Search Tree",
 }
 
-export const DSList: FC<{
-  setSelectedDS: React.Dispatch<React.SetStateAction<DS>>;
-}> = ({ setSelectedDS }) => {
+export const DSList: FC = () => {
   const list = Object.entries(DS);
   const buttonList = list.map(([key, value], index) => (
-    <button
-      key={key}
-      className={`
+    <Link key={key} to={`/${value.split(" ").join("-")}`}>
+      <button
+        className={`
           w-full p-2 border-y-slate-100 font-bold bg-slate-600 text-white hover:bg-slate-800
           ${index === 0 ? " rounded-t-lg" : ""}
           ${index === list.length - 1 ? " rounded-b-lg" : ""}
         `}
-      onClick={() => setSelectedDS(value)}
-    >
-      {value}
-    </button>
+      >
+        {value}
+      </button>
+    </Link>
   ));
 
   return <div className="mt-2">{buttonList}</div>;
